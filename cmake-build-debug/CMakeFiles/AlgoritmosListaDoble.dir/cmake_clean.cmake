c1f75d2aa7618d295348c96ac3216b5d3f2e7ccb
Chip <nicolas.chipana@utec.edu.pe>
2018-09-03 22:06:54 -0500

    Añadiendo funciones

---
+++ cmake-build-debug/CMakeFiles/AlgoritmosListaDoble.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AlgoritmosListaDoble.dir/main.cpp.o"
+  "AlgoritmosListaDoble.pdb"
+  "AlgoritmosListaDoble"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgoritmosListaDoble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
